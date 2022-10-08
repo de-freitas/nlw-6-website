de9a3f3f18e48cffc74f99580dcd396df27efa33
unknown <claudio.g.freitas@outlook.com>
2022-10-08 15:10:47 -0300

    Create services page and add some functionalities in JS

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
- 
+// abre e fecha o menu quando clicar no ícone //
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+    element.addEventListener('click', function() {
+        nav.classList.toggle('show')
+    })
+}
+
+// quando clicar em algum item do menu, escondê-lo //
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+    link.addEventListener('click', function() {
+        nav.classList.remove('show')
+    })
+}
+
+// mudar o header (inserir classe scroll) quando rolar a página //
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function() {
+    if (this.window.scrollY >= navHeight) {
+        header.classList.add('scroll')
+    } else {
+        header.classList.remove('scroll')
+    }
+})
