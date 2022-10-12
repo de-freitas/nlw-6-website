644a417bf435a8391311d6f268edf25b1f157186
unknown <claudio.g.freitas@outlook.com>
2022-10-12 14:15:47 -0300

    Finish mobile page

--- script.js
+++ script.js
@@ -18,13 +18,59 @@ for (const link of links) {
 }
 
 // mudar o header (inserir classe scroll) quando rolar a página //
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function() {
+function changeHeaderWhenScroll() {
+    const header = document.querySelector('#header')
+    const navHeight = header.offsetHeight
+
     if (this.window.scrollY >= navHeight) {
         header.classList.add('scroll')
     } else {
         header.classList.remove('scroll')
     }
+}
+
+// swiper || carousel
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    pagination: {
+        el: '.swiper-pagination'
+    },
+    mousewheel: true,
+    keyboard: true
+});
+
+// ScrollReveal
+const scrollReveal = ScrollReveal( {
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+    reset: true
+})
+
+scrollReveal.reveal(
+    `#home .text, #home .image,
+    #about .image, #about .text,
+    #services header, #services .card,
+    #testimonials header, #testimonials .testimonials,
+    #contact .text, #contact .links,
+    footer .brand, footer .social`, 
+    { interval: 100 } 
+)
+
+// botão back-to-top
+function backToTopWhenScroll() {
+    const backToTopButton = document.querySelector('.back-to-top')
+
+    if (this.window.scrollY >= 560) {
+        backToTopButton.classList.add('show')
+    } else {
+        backToTopButton.classList.remove('show')
+    }
+}
+
+// eventListeners when Scroll
+window.addEventListener('scroll', function() {
+    changeHeaderWhenScroll()
+    backToTopWhenScroll()
 })
